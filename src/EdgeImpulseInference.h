59ccc0057d6806309f393386d4dc134a083b29fe
root <root@a0d7b954-ssh.local.hass.io>
2025-06-08 13:09:33 -0400

    Add include guards to prevent multiple definitions

--- src/EdgeImpulseInference.h
+++ src/EdgeImpulseInference.h
@@ -3,6 +3,9 @@
 // Edge Impulse Inference Library for ESP32
 // Clean interface header for ESPHome integration
 
+#ifndef EDGE_IMPULSE_INFERENCE_H
+#define EDGE_IMPULSE_INFERENCE_H
+
 #include "edge-impulse-sdk/classifier/ei_run_classifier.h"
 #include "edge-impulse-sdk/dsp/numpy.hpp"
 
@@ -10,3 +13,5 @@
 extern "C" {
     EI_IMPULSE_ERROR run_classifier(signal_t *signal, ei_impulse_result_t *result, bool debug);
 }
+
+#endif // EDGE_IMPULSE_INFERENCE_H
