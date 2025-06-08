4cb42003229c509dca2fa37a539cb2e062de9c24
root <root@a0d7b954-ssh.local.hass.io>
2025-06-08 12:28:55 -0400

    Add complete Edge Impulse SDK for ESP32 (no CMSIS) + trained model

---
+++ src/EdgeImpulseInference.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Edge Impulse Inference Library for ESP32
+// Clean interface header for ESPHome integration
+
+#include "edge-impulse-sdk/classifier/ei_run_classifier.h"
+#include "edge-impulse-sdk/dsp/numpy.hpp"
+
+// Make core functions available
+extern "C" {
+    EI_IMPULSE_ERROR run_classifier(signal_t *signal, ei_impulse_result_t *result, bool debug);
+}
